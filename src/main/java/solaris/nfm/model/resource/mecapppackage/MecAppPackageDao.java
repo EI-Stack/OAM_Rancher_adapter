a14430f4f3ae26692ab65c61e3b5c23d63af9a90
WeiChiang <wejohnwang@gmail.com>
2024-12-30 14:18:44 +0800

    Delete some files

--- src/main/java/solaris/nfm/model/resource/mecapppackage/MecAppPackageDao.java
+++
@@ -1,8 +0,0 @@
-package solaris.nfm.model.resource.mecapppackage;
-
-import solaris.nfm.model.base.repository.DaoBase;
-
-public interface MecAppPackageDao extends DaoBase<MecAppPackage, Long>
-{
-	Long countByName(String name);
-}
